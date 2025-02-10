371e50009705b0fb812cec175f66fc57c845b4ed
Klein Petr <petr@kleinpetr.com>
2025-02-10 15:58:54 +0100

    build(sdk): setup build config, add changelogen

---
+++ packages/workers-sdk/build.config.ts
@@ -0,0 +1,25 @@
+import { defineBuildConfig } from 'unbuild'
+
+export default defineBuildConfig({
+  entries: ['./src/index'],
+  outDir: 'dist',
+  declaration: true,
+  rollup: {
+    emitCJS: true,
+  },
+  externals: [
+    'cloudflare:workers',
+  ],
+  // externals: [
+  //   'drizzle-kit',
+  //   'drizzle-orm',
+  //   'zod',
+  //   'consola',
+  //   '@types/node',
+  //   '@cloudflare/workers-types',
+  //   'cloudflare:workers',
+  // ],
+  // dependencies: [
+  //   '@cloudflare/workers-types/experimental',
+  // ],
+})
---
+++ packages/workers-sdk/src/index.ts
@@ -0,0 +1,18 @@
+import * as Database from './database/schema'
+import * as Types from './types'
+import * as Utils from './utils'
+import * as Workers from './workers'
+
+// Module
+
+// Database
+export { Database }
+
+// Types
+export { Types }
+
+// Utils
+export { Utils }
+
+// Workers
+export { Workers }
