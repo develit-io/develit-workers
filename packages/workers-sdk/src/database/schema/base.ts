46da05a5c282e0abb5db5ce4e97245dd7a1873b5
FF <filip.f@develit.io>
2025-02-20 13:50:07 +0100

    feat: update ModifiedAt column when updating row (#3)

--- packages/workers-sdk/src/database/schema/base.ts
+++ packages/workers-sdk/src/database/schema/base.ts
@@ -4,5 +4,5 @@ import { integer, text } from 'drizzle-orm/sqlite-core'
 export const base = {
   id: text('id').primaryKey(),
   createdAt: integer('created_at', { mode: 'timestamp_ms' }).default(sql`(unixepoch())`),
-  modifiedAt: integer('modified_at', { mode: 'timestamp_ms' }),
+  modifiedAt: integer('modified_at', { mode: 'timestamp_ms' }).default(sql`null`).$onUpdate(() => new Date()),
 }
