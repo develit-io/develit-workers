556c8bd8b92d54ab45224e296ee15a713e40bca3
ffdevelit <filip.f@develit.io>
2025-02-27 17:41:35 +0100

    fix: createdAt column creates Unix timestamp in seconds

--- packages/workers-sdk/src/database/schema/base.ts
+++ packages/workers-sdk/src/database/schema/base.ts
@@ -3,6 +3,6 @@ import { integer, text } from 'drizzle-orm/sqlite-core'
 
 export const base = {
   id: text('id').primaryKey(),
-  createdAt: integer('created_at', { mode: 'timestamp_ms' }).default(sql`(unixepoch())`),
+  createdAt: integer('created_at', { mode: 'timestamp_ms' }).default(sql`(unixepoch('subsec'))`),
   modifiedAt: integer('modified_at', { mode: 'timestamp_ms' }).default(sql`null`).$onUpdate(() => new Date()),
 }
