bb6c3e8024d656aaf00f431c770d44e7e13251d8
Klein Petr <petr@kleinpetr.com>
2025-04-07 15:18:59 +0200

    feat(sdk): add basic logging abstraction

--- packages/workers-sdk/src/workers/develitWorkerEntrypoint.ts
+++ packages/workers-sdk/src/workers/develitWorkerEntrypoint.ts
@@ -1,7 +1,16 @@
 import { WorkerEntrypoint } from 'cloudflare:workers'
 
 export abstract class DevelitWorkerEntrypoint<TEnv> extends WorkerEntrypoint<TEnv> {
+  protected name: string = 'not-set'
+
   async fetch() {
     return new Response('Service is up and running!')
+  }
+
+  log(data: object) {
+    console.log({
+      entrypoint: this.name,
+      ...data,
+    })
   }
 }
