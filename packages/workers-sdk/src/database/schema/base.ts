e6b4249748e70e7206f96037294fca5779d94b9b
ffdevelit <filip.f@develit.io>
2025-04-21 17:21:27 +0200

    refactor: adjust base db columns

--- packages/workers-sdk/src/database/schema/base.ts
+++ packages/workers-sdk/src/database/schema/base.ts
@@ -1,8 +1,7 @@
-import { sql } from 'drizzle-orm'
 import { integer, text } from 'drizzle-orm/sqlite-core'
 
 export const base = {
   id: text('id').primaryKey(),
-  createdAt: integer('created_at', { mode: 'timestamp_ms' }).default(sql`(unixepoch('subsec') * 1000)`),
-  modifiedAt: integer('modified_at', { mode: 'timestamp_ms' }).default(sql`null`).$onUpdate(() => new Date()),
+  createdAt: integer('created_at', { mode: 'timestamp_ms' }).notNull(),
+  modifiedAt: integer('modified_at', { mode: 'timestamp_ms' }).$onUpdate(() => new Date()),
 }
