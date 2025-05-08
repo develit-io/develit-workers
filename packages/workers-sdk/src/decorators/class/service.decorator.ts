1841033489701edae26b3b9bc32d896d6cb0d68b
Daniel Klein <daniel@develit.io>
2025-05-08 19:18:24 +0200

    feat(sevice-decorator): add suffix

--- packages/workers-sdk/src/decorators/class/service.decorator.ts
+++ packages/workers-sdk/src/decorators/class/service.decorator.ts
@@ -1,8 +1,8 @@
-export const service = (name: string) => {
+export const service = (serviceName: string) => {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   return function <T extends new (...args: any[]) => object>(constructor: T) {
     return class extends constructor {
-      name = name
+      name = `${serviceName}-service`
     }
   }
 }
